f29291dc8cbfdfd93452c2f9f99c77b4e2675d17
Davereks <Davereks@gmail.com>
2022-10-18 08:21:41 -0700

    vested loops 2

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -7,10 +7,11 @@
  */
 int _abs(int c)
 {
-        if (c > 0 || c == 0)
-        {
-        return (c);
-        }
-        else
-        return (c * -1);
+	if (c > 0 || c == 0)
+	{
+	return (c);
+	}
+	else
+	return (c * -1);
 }
+
