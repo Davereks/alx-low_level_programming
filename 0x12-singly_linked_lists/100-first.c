336d40c0b345ca5f2eed7777e15532c64192f9a3
Davereks <Davereks@gmail.com>
2022-11-24 15:50:18 -0800

    singly_linked_lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void Obstacles(void) __attribute__ ((constructor));
+/**
+ * Obstacles - The greater, the greater the glory in overcoming it.
+ */
+void Obstacles(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
